0208deb425bb5128a6d0f7b95e11c3f7d5fc92de
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-02-19 09:40:59 -0500

    Prueba1

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Asiento {
+	String color;
+	int precio;
+	public int registro;
+	
+	public void cambiarColor(String color) {
+		if (color == "amarillo" || color == "rojo" || color =="verde"|| color =="negro"||color=="blanco" ) {
+				this.color=color;
+		}
+	}
+	
+	
+	
+}
