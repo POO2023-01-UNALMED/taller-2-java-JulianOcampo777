af29c9744e6f708f9ba9da6c98d4947ab5085a96
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-02-19 11:09:37 -0500

    p4

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,16 +1,13 @@
 package test;
-
 public class Asiento {
-	String color;
-	int precio;
-	public int registro;
-	
-	public void cambiarColor(String color) {
-		if (color == "amarillo" || color == "rojo" || color =="verde"|| color =="negro"||color=="blanco" ) {
-				this.color=color;
-		}
-	}
-	
-	
-	
+    String color;
+    int precio;
+    int registro;
+
+
+    void cambiarColor(String color){
+        if(  color.equals("rojo") || color.equals("verde") ||  color.equals("blanco") || color.equals("amarillo") || color.equals("negro")){
+            this.color = color;
+        }
+    }
 }
