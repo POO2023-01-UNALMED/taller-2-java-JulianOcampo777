5f6807bcdaae128670f18671ba0fe3f9b6dfffb7
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-02-19 11:11:24 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,25 +1,40 @@
 package test;
-
 public class Auto {
-	String modelo;
-	int precio;
-	Asiento asientos[];
-	String marca;
-	Motor motor;
-	int registro;
-	static int cantidadCreados;
-	
-	public int cantidadAsientos() {
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    int cantidadAsientos(){
         int cantidad = 0;
-        for (Asiento asiento : asientos) {
-            if (asiento != null) {
+        for(Asiento a : this.asientos){
+            if(a!= null){
                 cantidad++;
             }
         }
         return cantidad;
     }
-	
-	
+
+    
+    
+    String verificarIntegridad(){
+        boolean bandera = true;
+        if ( this.registro == motor.registro){
+            for(Asiento asiento: this.asientos){
+                if (asiento != null){
+                	
+                	if(asiento.registro != this.registro){
+                		bandera = false;
+                		break;
+                	}
+                }
+            }
+        }
+        return bandera? "Auto original" : "Las piezas no son originales";
+    }
 
 }
 
